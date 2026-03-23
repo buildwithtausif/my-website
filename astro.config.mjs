4c066a8ec678c8838fd63879e90afa27542f2bfa
Tausif Alam <devops.tausifalam@outlook.com>
2026-03-24 01:20:51 +0530

    addition: support for content-collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,9 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:4321/',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react(), mdx()]
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,19 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection } from "astro/content/config";
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
+// articles
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/pages/articles" }),
+  loader: glob({ 
+    pattern: '**/*.{md,mdx}',
+    base: 'src/content/blog',
+  }),
   schema: z.object({
     title: z.string(),
+    author: z.string(),
     description: z.string(),
-    pubDate: z.coerce.date(),
+    tags: z.array(z.string().nonempty()),
+    pubDate: z.date()
   })
 });
 
